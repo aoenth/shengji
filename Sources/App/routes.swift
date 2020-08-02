89511c7d60e5470ec39f8e472c69596dedb9486a
Kevin <peng.kevin@me.com>
2020-08-02 03:00:00 -0400

    Echo using WebSocket

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,10 @@
 import Fluent
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+func socketRoutes(_ app: Application) throws {
+    app.webSocket("echo") { (req, ws) in
+        ws.onText { ws, text in
+            ws.send(text)
+        }
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
 }
