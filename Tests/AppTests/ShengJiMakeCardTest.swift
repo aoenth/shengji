7965a5862af0b2d01388383ab91d65a895496262
Kevin <peng.kevin@me.com>
2020-08-02 19:12:56 -0400

    Adjust spelling

--- Tests/AppTests/ShengJiMakeCardTest.swift
+++ Tests/AppTests/ShengJiMakeCardTest.swift
@@ -1,5 +1,5 @@
 //
-//  ShenJiMakeCardTest.swift
+//  ShengJiMakeCardTest.swift
 //  AppTests
 //
 //  Created by Kevin Peng on 2020-08-02.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import App
 
-class ShenJiMakeCardTest: XCTestCase {
+class ShengJiMakeCardTest: XCTestCase {
 
     func testAceOfSpades() {
         let card = Card(id: 8)
@@ -63,4 +63,50 @@ class ShenJiMakeCardTest: XCTestCase {
         let expectation = Card(suit: .rightBower, rank: .rightBower, deckNumber: 2)
         XCTAssertEqual(card, expectation)
     }
+    
+    func testAceOfSpadesID() {
+        let card = Card(id: 8)
+        XCTAssertEqual(card.cardID, 8)
+    }
+    
+    func testAceOfSpadesDeck2ID() {
+        let card = Card(id: 12)
+        XCTAssertEqual(card.cardID, 12)
+    }
+    
+    func testKingOfHeartsID() {
+        let card = Card(id: 105)
+        XCTAssertEqual(card.cardID, 105)
+    }
+    
+    func testKingOfHeartsDeck2ID() {
+        let card = Card(id: 109)
+        XCTAssertEqual(card.cardID, 109)
+    }
+    
+    func test6OfDiamondsID() {
+        let card = Card(id: 50)
+        XCTAssertEqual(card.cardID, 50)
+    }
+    
+    func testLeftBowerID() {
+        let card = Card(id: 112)
+        XCTAssertEqual(card.cardID, 112)
+    }
+    
+    func testLeftBowerDeck2ID() {
+        let card = Card(id: 113)
+        XCTAssertEqual(card.cardID, 113)
+    }
+    
+    func testRightBowerID() {
+        let card = Card(id: 114)
+        XCTAssertEqual(card.cardID, 114)
+    }
+    
+    func testRightBowerDeck2ID() {
+        let card = Card(id: 115)
+        XCTAssertEqual(card.cardID, 115)
+    }
+    
 }
